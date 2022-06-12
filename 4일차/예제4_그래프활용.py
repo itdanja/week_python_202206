627d905dea685c582a9322e7f27bba49ee321307
류정원 <ryuzy@kakao.com>
2022-06-12 12:21:05 +0900

    4일차

---
+++ 4일차/예제4_그래프활용.py
@@ -0,0 +1,13 @@
+# 최단 거리 찾기 [ 그래프 구현 ]
+
+# 1. 그래프 클래스 선언 [2차원 리스트를 이용한 그래프 구현 ]
+class Graph() :
+    def __init__(self , size):
+        self.SIZE = size
+        self.Graph = [ [0 for _ in range(size)] for _ in range(size) ]
+
+
+
+
+
+
